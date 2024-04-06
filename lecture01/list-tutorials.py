07e7fe06a1f035aaff069bd17e90477f2b02077e
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-04-06 14:08:39 +0530

    Second Half

---
+++ lecture01/list-tutorials.py
@@ -0,0 +1,43 @@
+# Approach 1 to store students records
+# name1 = "abc"
+# roll_no1 = 1
+# address1 = 'Ahmedabad'
+# father_name1 = "F1"
+
+# name2 = "xzy"
+# rollno2 = 2
+# address_2 = 'Ahmedabad'
+# father_name2 = "F2"
+
+# Approach 2 to store students records
+#          0     1      2     3
+names = ['abc','xyz', 'lm', 'kyp']
+print(names)
+
+print(names[0])
+# print(names[1])
+print(names[2])
+print(names[3])
+
+temperatures = [30, 32, 45, 34.2]
+
+print(temperatures)
+print(temperatures[1])
+
+
+data = ["String1", 31, "String2", 83.1]
+
+print(data)
+print(data[2])
+
+
+marks = [
+    [30, 20, 90],
+    [40, 10, 45],
+    [40, "Gujarati"],
+]
+
+print(marks)
+
+print(marks[0][1])
+print(marks[2][1])
---
+++ lecture01/student_management_system.py
@@ -0,0 +1,14 @@
+# Name, Address, Father's Name, Roll No
+
+roll_nos = [1, 2, 3, 4]
+
+names = ["ABC", "XYZ", "LKML", "ASJ"]
+
+addresses = ["A", "B", "C", "D"]
+
+father_names = ["F1", "F2", "F3", "F4"]
+
+print(roll_nos)
+print(names)
+print(addresses)
+print(father_names)
