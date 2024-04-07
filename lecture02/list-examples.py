55bf953bf9bb7264aa83bcabc3693e6fbab4f3b6
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-04-07 12:38:11 +0530

    First half

---
+++ lecture02/list-examples.py
@@ -0,0 +1,46 @@
+abc = 'mohit'       # This was written to prevent error
+
+names = [
+    abc,
+    'abc',
+    'xyz',
+    'lmn',
+    'absc',
+]
+
+# names[2] = 'xyyz'           # Updating to latest value
+
+print(names)
+
+# print(names[-2])
+
+
+# new_names = [
+#     names[0],
+#     names[1],
+#     names[2],
+# ]
+
+# new_names = names[0:3]
+# new_names2 = names[-1:-3:-1]
+
+# print(new_names)
+# print(new_names2)
+
+
+# names.append('zeserg', 6)
+# names.append('ANSKDAS')
+# names.append([23456, 766])
+# names.extend([891])
+
+# names.insert(1, "Emma Watson")
+# names.pop(3)
+names.remove('abc')
+print()
+names[-2:len(names):-1]
+print(len(names))
+print(len(abc))
+# print(names)
+
+
+# print(names.index('nameaksdkhasd'))
---
+++ lecture02/tuple-examples.py
@@ -0,0 +1,12 @@
+l = [1, 2, -13, "ajsd", ("j", 9, "5j")]
+
+t = (1, 2, -13, "ajsd", 3.14, ("j", 9, "5j"))
+
+print(l)
+print(t)
+
+l[0] = "Emma"
+t[0] = "Emma"
+
+print(l)
+print(t)
