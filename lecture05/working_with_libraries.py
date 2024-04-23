25a906c94335ad0ff3b347d6969dd12ace341f82
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-04-23 07:35:28 +0530

    Added Homework

---
+++ lecture05/working_with_libraries.py
@@ -0,0 +1,16 @@
+import math                 # Whole library is imported
+
+
+x = math.exp(5)             # returns e^5 
+print(x)
+
+from math import exp        # Only function is imported
+
+y = exp(5)                  # returns e^5
+print(y)
+
+
+from os import listdir
+
+
+print(listdir())
